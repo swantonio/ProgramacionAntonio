13b0802353f96b9f530eaa690ba277357a168c25
swantonio <antonio13dltm@gmail.com>
2025-10-17 12:51:38 +0200

    Ejercicios Tema 2 del 1-18

--- src/Tema2/Tema2Ejercicio24.java
+++ src/Tema2/Tema2Ejercicio24.java
@@ -7,13 +7,24 @@ public class Tema2Ejercicio24 {
         int aprobados = 0;
         int suspendidos = 0;
         double nota = 0;
-        int fin = -1;
         Scanner in = new Scanner(System.in);
-        System.out.println("Introduce una nota");
-        if (nota == fin){
-        while (nota <0 && nota >10) {
-            if (nota <)
+        System.out.println("Introduce notas, cuando quieras acabar introduce -1");
+        nota = in.nextDouble();
+        while (nota != -1) {
+            if (nota < 0 || nota > 10) {
+                System.out.println("Error introduce una nota valida ");
+            } else {
+                if (nota <= 4.9) {
+                    suspendidos++;
+                } else {
+                    aprobados++;
+                }
+            }
+            System.out.println("Introduce otra nota");
+            nota = in.nextDouble();
         }
 
+            System.out.println("La cantidad de aprobados es de " + aprobados + " y la de suspendidos " + suspendidos);
+            in.close();
+        }
     }
-}
