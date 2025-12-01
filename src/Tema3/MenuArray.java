3ba7cbe1aa012de2bb74aad8dc9b69e5c63ad166
swantonio <antonio13dltm@gmail.com>
2025-12-01 10:54:59 +0100

    Ejercicios Tema 3

--- src/Tema3/MenuArray.java
+++ src/Tema3/MenuArray.java
@@ -1,5 +1,6 @@
 package Tema3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MenuArray {
@@ -13,7 +14,56 @@ public class MenuArray {
             case 'a':
                 int [] devueltaArray = new int[] {1,2,3,4,5,6,7,8,9,10};
                 MyArray.devolverArray(devueltaArray);
+                break;
             case 'b':
+                int [] mayorArray = new int [] {1,2,3,4,5,6,7,8,9,10};
+                System.out.println("El numero mayor del array es: " + MyArray.mayorArray(mayorArray));
+                break;
+            case 'c':
+                int [] menorArray = new int [] {1,2,3,4,5,6,7,8,9,10};
+                System.out.println("El numero menor del array es: " + MyArray.menorArray(menorArray));
+                break;
+            case 'd':
+                int [] mediaArray = new int [] {1,2,3,4,5,6,7,8,9,10};
+                System.out.println("La media del array es: " + MyArray.mediaArray(mediaArray));
+                break;
+            case 'e':
+                int [] elementArray = new int [] {1,2,3,4,5,6,7,8,9,10};
+                System.out.println("Introduce un numero para comprobar si esta en el array");
+                int numeroelemento = in.nextInt();
+                boolean existencia = MyArray.elementoArray(elementArray, numeroelemento);
+                if (existencia) {
+                    System.out.println("El numero esta en el array");
+                } else {
+                    System.out.println("El numero no esta en el array");
+                }
+            case 'f':
+                int [] array1 = new int [] {1,2,3,4,5,6,7,8,9,10};
+                int [] array2 = new int [] {1,2,3,4,5,6,7,8,9,10};
+                int[] resultado = MyArray.sumaVectores(array1, array2);
+                System.out.println("La suma de los vectores es: " + Arrays.toString(resultado));
+                break;
+            case 'g':
+                int [] array1resta = new int [] {2,4,6,8,10,12,14,16,18,20};
+                int [] array2resta = new int [] {1,2,3,4,5,6,7,8,9,10};
+                int [] resultadoresta = MyArray.restaVectores(array1resta, array2resta);
+                System.out.println("La resta de los vectores es: " + Arrays.toString(resultadoresta));
+                break;
+            case 'h':
+                int [] producto1 = new int [] {1,2,3,4,5,6,7,8,9,10};
+                int [] producto2 = new int [] {1,2,3,4,5,6,7,8,9,10};
+                int resultadoproducto = MyArray.productoVectores(producto1, producto2);
+                System.out.println("El producto escalado es: " + resultadoproducto);
+                break;
+            case 'i':
+                int [] inversion = new int [] {1,2,3,4,5,6,7,8,9,10};
+                int[] resultadoinversion = MyArray.inversionArray(inversion);
+                System.out.println("El array invertido es: " + Arrays.toString(resultadoinversion));
+                break;
+            case 'j':
+                int [] inversion2 = new int [] {1,2,3,4,4,5,6,7,8,9,10};
+                MyArray.inversionArray2(inversion2);
+                System.out.println("El array invertido en procedimiento es: " + Arrays.toString(inversion2));
         }
     }
 
@@ -27,7 +77,7 @@ public class MenuArray {
         System.out.println("Introduce g para restar 2 vectores del array");
         System.out.println("Introduce h para saber el producto de 2 vectores del array");
         System.out.println("Introduce i si quieres invertir el orden del array");
-        System.out.println("Introduce j si quieres invertir del orden del array sin devolver nada");
+        System.out.println("Introduce j si quieres invertir del orden del array con un procedimiento");
         System.out.println("Introduce k si quieres saber si el array es capicua");
     }
 }
--- src/Tema3/MyArray.java
+++ src/Tema3/MyArray.java
@@ -1,11 +1,8 @@
 package Tema3;
 
-import java.util.Arrays;
-
 public class MyArray {
 
     public static void devolverArray(int[] devueltaArray) {
-        //System.out.println(Arrays.toString(devueltaArray));
         //for (int i = 0; i < devueltaArray.length; i++)
         //{
         //    System.out.println(devueltaArray[i]);
@@ -14,4 +11,82 @@ public class MyArray {
             System.out.println(j);
         }
     }
-}
+
+    public static int mayorArray(int[] mayorArray) {
+        int max = mayorArray[1];
+        for (int i = 0; i < mayorArray.length; i++) {
+            if (mayorArray[i] > max) {
+                max = mayorArray[i];
+            }
+        }
+        return max;
+    }
+
+    public static int menorArray(int[] menorArray) {
+        int menor = menorArray[1];
+        for (int i = 0; i < menorArray.length; i++) {
+            if (menorArray[i] < menor) {
+                menor = menorArray[i];
+            }
+        }
+        return menor;
+    }
+
+    public static int mediaArray(int[] mediaArray) {
+        int sumamedia = 0;
+        for (int i = 0; i < mediaArray.length; i++) {
+            sumamedia = sumamedia + mediaArray[i];
+        }
+        return sumamedia / mediaArray.length;
+    }
+
+    public static boolean elementoArray(int[] elementoArray, int numeroelemento) {
+        for (int i = 0; i < elementoArray.length; i++) {
+            if (elementoArray[i] == numeroelemento) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static int []sumaVectores(int [] a, int [] b) {
+        int [] resultado = new int [a.length];
+        for (int i = 0; i < resultado.length; i++) {
+            resultado[i] = a[i] + b[i];
+        }
+        return resultado;
+    }
+    public static int []restaVectores(int [] a, int [] b){
+        int [] resultado = new int[a.length];
+        for (int i = 0; i < resultado.length; i++) {
+            resultado[i] = a[i] - b[i];
+        }
+        return resultado;
+    }
+    public static int productoVectores (int [] a, int [] b) {
+        int resultado= 0;
+        for (int i = 0; i < a.length; i++) {
+            resultado += a[i] * b[i];
+        }
+        return resultado;
+    }
+    public static int[] inversionArray(int[] a) {
+        int[] resultado = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            resultado[i] = a[a.length - 1 - i];
+        }
+        return resultado;
+    }
+    public static void inversionArray2 (int [] inversionArray2) {
+            int inicio = 0;
+            int fin = inversionArray2.length - 1;
+            while (inicio < fin) {
+                int temp = inversionArray2[inicio];
+                inversionArray2[inicio] = inversionArray2[fin];
+                inversionArray2[fin] = temp;
+                inicio++;
+                fin--;
+            }
+        }
+    public static boolean capicuaArray (int [] capicuaArray) {
+    }
+    }
