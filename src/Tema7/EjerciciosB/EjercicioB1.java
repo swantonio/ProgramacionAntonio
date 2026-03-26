ef6cc7812ebdbcd61810722c5e684c5c4e1a6e26
swantonio <antonio13dltm@gmail.com>
2026-03-26 09:44:16 +0100

    Tema7

--- src/Tema7/EjerciciosB/EjercicioB1.java
+++ src/Tema7/EjerciciosB/EjercicioB1.java
@@ -3,11 +3,7 @@ package Tema7.EjerciciosB;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Stream;
+
 
 public class EjercicioB1 {
     public static void main(String[] args) throws IOException {
